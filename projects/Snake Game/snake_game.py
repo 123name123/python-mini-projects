20e80b69a8b51d4169fdc5b4f146a95871828743
el1sey <muxin-2002@mail.ru>
2022-09-24 01:15:26 +0300

    update change_direction

--- projects/Snake Game/snake_game.py
+++ projects/Snake Game/snake_game.py
@@ -29,8 +29,7 @@ class Snake:
 class Food:
 
     def __init__(self):
-
-        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
         y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
 
         self.coordinates = [x, y]
@@ -39,7 +38,6 @@ class Food:
 
 
 def next_turn(snake, food):
-
     x, y = snake.coordinates[0]
 
     if direction == "up":
@@ -85,25 +83,13 @@ def next_turn(snake, food):
 
 
 def change_direction(new_direction):
-
     global direction
-
-    if new_direction == 'left':
-        if direction != 'right':
-            direction = new_direction
-    elif new_direction == 'right':
-        if direction != 'left':
-            direction = new_direction
-    elif new_direction == 'up':
-        if direction != 'down':
-            direction = new_direction
-    elif new_direction == 'down':
-        if direction != 'up':
-            direction = new_direction
+    sides = {"left": -1, 'right': 1, "up": 2, "down": -2}
+    if sides[direction] == -sides[new_direction] and sides[direction] + sides[new_direction] == 0:
+        direction = new_direction
 
 
 def check_collisions(snake):
-
     x, y = snake.coordinates[0]
 
     if x < 0 or x >= GAME_WIDTH:
@@ -119,10 +105,9 @@ def check_collisions(snake):
 
 
 def game_over():
-
     canvas.delete(ALL)
-    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
-                       font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
+    canvas.create_text(canvas.winfo_width() / 2, canvas.winfo_height() / 2,
+                       font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover")
 
 
 window = Tk()
@@ -145,8 +130,8 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2) - (window_width/2))
-y = int((screen_height/2) - (window_height/2))
+x = int((screen_width / 2) - (window_width / 2))
+y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
